52d01e9dd50be2af7fe8e8346f2369465f543f04
Varun Jalandery <varun.jalandery@gmail.com>
2025-07-08 23:28:05 +0200

    Create setup_sarah.sh

---
+++ setup_sarah.sh
@@ -0,0 +1,16 @@
+sudo apt-get update &&
+sudo apt -y install unzip  &&
+sudo apt -y install python3  &&
+sudo apt -y install python3.10-venv  &&
+curl -fsSL https://ollama.com/install.sh | sh  &&
+ollama pull mistral:7b &&
+ollama pull gemma3:4b  &&
+ollama pull gemma3:27b &&
+ollama pull gemma3n:e4b &&
+wget https://github.com/varun-jalandery/sarah/archive/refs/heads/mainline.zip  &&
+unzip mainline.zip  &&
+cd sarah-mainline  &&
+python3 -m venv .venv  &&
+source .venv/bin/activate && 
+python -m pip install -r requirements.txt &&
+./run_app.sh
